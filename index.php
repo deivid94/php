4f1d08c16543e1aad3d075c807a109a1e3d1f592
deivid94 <md031194@gmail.com>
2019-05-14 21:04:04 -0300

    AGORA SIM ESTA CERTO

--- index.php
+++ index.php
@@ -3,8 +3,5 @@
 echo "<strong>ola mundo </strong> IHHHUL DESGRAÇADA";
 <<<<<<< HEAD
 echo "duda loira boa"
-=======
 
-echo "delicia de rabo em morena";
->>>>>>> 858a00d81a504e4e689f12286a4b64175ad142eb
 ?>
